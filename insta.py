e1c891172e18c14fa5cec844dcb41e14bed3a116
horseman7677 <70053149+horseman7677@users.noreply.github.com>
2020-11-28 21:52:42 +0530

    Create insta.py
    download instagram dp

---
+++ insta.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import shutil
+import os
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+}
+
+INSTA_URL = 'https://www.instagram.com/'
+USER_ID = input('Enter the USER NAME : ')
+tail = "/?__a=1"
+
+URL = INSTA_URL+USER_ID+tail
+
+response = requests.get(URL, headers=header).json()
+
+image_location = response["graphql"]["user"]["profile_pic_url"]
+hd_image_location = response["graphql"]["user"]["profile_pic_url_hd"]
+
+image_location_response = requests.get(image_location, stream=True)
+with open("standard_img.jpg", "wb") as out_file:
+    shutil.copyfileobj(image_location_response.raw, out_file)
+
+hd_image_response = requests.get(hd_image_location, stream=True)
+with open("hd_img.jpg", "wb") as out_file:
+    shutil.copyfileobj(hd_image_response.raw, out_file)
